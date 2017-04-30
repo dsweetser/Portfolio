e77117e13788d66085171b196fccbb8bcfd6aec9
dsweetser <daniel.sweetser@gmail.com>
2017-04-30 09:43:41 -0400

    deploy task

---
+++ public/pictures.js
@@ -0,0 +1,8 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "a637d741b26554f73cf709d3fa4b43d4.jpg";
+
+/***/ }
+]);
